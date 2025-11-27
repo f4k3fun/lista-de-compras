6b40aaf5af6e26b36b99a8b6ad6d26a6409a7db6
Iago Guedes <f4k3fun@gmail.com>
2025-11-27 20:38:50 -0300

    adding functions in buttons with js for add more products

---
+++ scripts.js
@@ -0,0 +1,73 @@
+const form = document.querySelector("form");
+const input = document.querySelector("#product");
+const list = document.querySelector("ul");
+
+
+// Tratando input para nao digitar numeros.
+input.addEventListener("input", () => {
+  const value = input.value
+
+  const hasNumberRegex = /\d+/g;
+
+  input.value = value.replace(hasNumberRegex, "");
+
+  if(hasNumberRegex.test(value)){
+    alert("Voce esta digitando um numero, por favor somente letras");
+  }
+});
+
+
+
+// Criando evento de submit do formulario.
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+
+  //Verificando se o valor do input nao esta vazio.
+  if(input.value){
+    console.log("criado");
+
+    // Chamando funcao para criar elemento HTML.
+    newProduct(input.value);
+    resetInput(input);
+  } else {
+    alert("Digite o nome do produto");
+  }
+})
+
+
+
+// Criando produto na lista de produtos
+function newProduct(valor){
+
+  const li = document.createElement("li");
+  li.classList.add("product");
+
+  const div = document.createElement("div");
+
+  const p = document.createElement("p");
+  p.textContent = valor;
+
+  const span = document.createElement("span");
+
+  const input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+
+  const img = document.createElement("img");
+  img.setAttribute("src", "assets/icons/Frame-3.svg");
+
+  li.append(div);
+  li.append(img);
+  div.append(span);
+  div.append(p);
+  span.append(input);
+
+
+  list.append(li);
+}
+
+
+// Criando reset de input.
+function resetInput(input){
+  input.value = "";
+}
